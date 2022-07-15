fc8c6cd6740f97f89b393b51261fc8dd9df42356
Nishant <31984100+pachchigarn@users.noreply.github.com>
2022-07-15 13:47:00 +0400

    Added Comment in the Get Feed Use Case.

--- app/src/main/java/com/nishantp/bonialcodingchallenge/feature/mainfeed/domain/use_case/GetFeed.kt
+++ app/src/main/java/com/nishantp/bonialcodingchallenge/feature/mainfeed/domain/use_case/GetFeed.kt
@@ -10,6 +10,13 @@ import com.nishantp.bonialcodingchallenge.feature.mainfeed.data.mapper.toBrochur
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
+/**
+ * Use case class that converts the DTO received from the API Response to a domain model.
+ * Following Business Rules applicable:
+ * 1. Display only items with contentType: "brochure" or "brochurePremium"
+ * 2. Filter for brochures closer than 5 kms
+ * 3. Sorts the list by arranging the list with shortest distance first.
+ * */
 class GetFeed(private val repository: FeedRepository) {
 
     suspend operator fun invoke(): Flow<DataState<List<Brochure>>> = flow {
